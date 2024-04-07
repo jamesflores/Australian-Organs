e97578ffd018d92219a041c4024cf494ae41c0eb
James Flores <james@jamesflores.net>
2024-04-07 21:00:33 +1000

    provide full urls in social share images

--- app/views.py
+++ app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-import requests
 from django.conf import settings
+from django.contrib.sites.shortcuts import get_current_site
 
 
 def index(request):
+    domain = request.scheme + '://' + get_current_site(request).domain
     context = {
-        'GOOGLE_MAPS_API_KEY': settings.GOOGLE_MAPS_API_KEY
+        'GOOGLE_MAPS_API_KEY': settings.GOOGLE_MAPS_API_KEY,
+        'domain': domain
     }
     return render(request, 'map/map.html', context)
