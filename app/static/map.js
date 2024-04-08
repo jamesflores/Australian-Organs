ad3ad752e50cd723e71e73f6ac1e6daa2d1613bd
James Flores <james@jamesflores.net>
2024-04-08 10:11:56 +1000

    refactor map.js

--- app/static/map.js
+++ app/static/map.js
@@ -1,10 +1,84 @@
 window.initMap = function() {
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 10,
-        center: {lat: -33.8688, lng: 151.2093}  // Sydney
+        center: {lat: -33.8688, lng: 151.2093}  // Sydney: default
     });
+    getUserLocation(map, updateMarkers);  // center the map on the user's location
 
-    // Try to get the user's location
+    map.addListener('dragend', function() {  // update markers when the map is dragged
+        updateMarkers(map);
+    });
+    
+    updateMarkers(map);  // initial markers
+}
+
+function updateMarkers(map) {
+    var center = map.getCenter();
+    var lat = center.lat();
+    var lon = center.lng();
+    var radius = 50;  // km
+
+    var currentInfoWindow = null;  // Keep track of the currently open info window
+    $.ajax({
+        url: "https://api.ohta.org.au/geosearch/",
+        data: {'lat': lat, 'lon': lon, 'radius': radius},
+        dataType: 'json',
+        success: function(data) {
+            data.forEach(function(organ) {
+                var marker = new google.maps.Marker({
+                    position: {lat: organ.latitude, lng: organ.longitude},
+                    map: map,
+                    title: organ.name,
+                    clicked: false
+                });
+
+                var infoWindow = new google.maps.InfoWindow({
+                    content: `
+                        <div class="row">
+                            <div class="col-md-4">
+                                <a href="${organ.url}" target="_blank">
+                                    <img src="${organ.image}" alt="${organ.name}" style="max-width:100%; max-height:100%;" loading="lazy">
+                                </a>
+                            </div>
+                            <div class="col-md-8">
+                                <a href="${organ.url}" target="_blank">
+                                    <h5>${organ.name}</h5>
+                                </a>
+                                <p>${organ.address}, ${organ.city}, ${organ.state} ${organ.postcode}</p>
+                                <p>${organ.description}</p>
+                            </div>
+                        </div>
+                    `
+                });
+
+                var clicked = false;
+                marker.addListener('mouseover', function() {
+                    // Close the current info window if it's open
+                    if (currentInfoWindow) {
+                        currentInfoWindow.close();
+                    }
+                    
+                    // Open the info window for the current marker
+                    this.clicked = true;
+                    infoWindow.open(map, marker);
+
+                    // Set the current info window to the one that was just opened
+                    currentInfoWindow = infoWindow;
+                });
+                marker.addListener('mouseout', function() {
+                    if (!clicked)
+                        infoWindow.close();
+                });
+                marker.addListener('click', function() {
+                    clicked = true;
+                    infoWindow.open(map, marker); 
+                });
+            });
+        }
+    });
+}
+
+function getUserLocation(map, updateMarkers) {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
             var userLocation = {
@@ -31,89 +105,17 @@ window.initMap = function() {
                 }
 
                 // Call updateMarkers after the map's center has been set
-                updateMarkers();
+                updateMarkers(map);
             });
         }, function() {
             // If the user denies the Geolocation request or it fails for some other reason, do nothing
             // The map will just stay centered on the default location
-            updateMarkers();
+            updateMarkers(map);
         });
     } else {
         // If Geolocation is not supported by the user's browser, call updateMarkers with the default location
-        updateMarkers();
+        updateMarkers(map);
     }
-
-    function updateMarkers() {
-        var center = map.getCenter();
-        var lat = center.lat();
-        var lon = center.lng();
-        var radius = 50;  // km
-
-        var currentInfoWindow = null;  // Keep track of the currently open info window
-        $.ajax({
-            url: "https://api.ohta.org.au/geosearch/",
-            data: {'lat': lat, 'lon': lon, 'radius': radius},
-            dataType: 'json',
-            success: function(data) {
-                data.forEach(function(organ) {
-                    var marker = new google.maps.Marker({
-                        position: {lat: organ.latitude, lng: organ.longitude},
-                        map: map,
-                        title: organ.name,
-                        clicked: false
-                    });
-
-                    var infoWindow = new google.maps.InfoWindow({
-                        content: `
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <a href="${organ.url}" target="_blank">
-                                        <img src="${organ.image}" alt="${organ.name}" style="max-width:100%; max-height:100%;" loading="lazy">
-                                    </a>
-                                </div>
-                                <div class="col-md-8">
-                                    <a href="${organ.url}" target="_blank">
-                                        <h5>${organ.name}</h5>
-                                    </a>
-                                    <p>${organ.address}, ${organ.city}, ${organ.state} ${organ.postcode}</p>
-                                    <p>${organ.description}</p>
-                                </div>
-                            </div>
-                        `
-                    });
-
-                    var clicked = false;
-
-                    marker.addListener('mouseover', function() {
-                        // Close the current info window if it's open
-                        if (currentInfoWindow) {
-                            currentInfoWindow.close();
-                        }
-                        
-                        // Open the info window for the current marker
-                        this.clicked = true;
-                        infoWindow.open(map, marker);
-
-                        // Set the current info window to the one that was just opened
-                        currentInfoWindow = infoWindow;
-                    });
-                    marker.addListener('mouseout', function() {
-                        if (!clicked)
-                            infoWindow.close();
-                    });
-                    marker.addListener('click', function() {
-                        clicked = true;
-                        infoWindow.open(map, marker); 
-                    });
-                });
-            }
-        });
-    }
-
-    map.addListener('dragend', function() {
-        updateMarkers();
-    });
-    updateMarkers();
 }
 
 window.onload = initMap;
