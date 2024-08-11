5692cecbabc5c749a507ce3b6a64d52ef314e757
James Flores <james@jamesflores.net>
2024-08-11 21:20:31 +1000

    add CustomUser model to admin

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
-from .models import Bookmark, URLRedirect
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Bookmark, URLRedirect, MagicLink
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'is_staff', 'is_active',)
+    list_filter = ('email', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
 
 class URLRedirectAdmin(admin.ModelAdmin):
     list_display = ('url', 'hit_count')
@@ -9,5 +29,6 @@ class BookmarkAdmin(admin.ModelAdmin):
     list_display = ('user', 'organ_id', 'created_at')
 
 
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(URLRedirect, URLRedirectAdmin)
 admin.site.register(Bookmark, BookmarkAdmin)
