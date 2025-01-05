5fc0e0c0d98a9915fde0700b30ff150a16caef71
James Flores <james@jamesflores.net>
2025-01-05 20:52:18 +1100

    use login codes instead of magic links

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser, Bookmark, URLRedirect, MagicLink
+from .models import CustomUser, Bookmark, URLRedirect
 
 
 class CustomUserAdmin(UserAdmin):
